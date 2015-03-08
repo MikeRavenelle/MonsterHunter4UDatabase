22f0572a1bc476bda6e23874b1a5ee0fefe05928
Mike Ravenelle <mike.ravenelle@gmail.com>
2015-03-08 18:50:26 -0500

    Preparing for iOS port

---
+++ app/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  test
+//
+//  Created by ilab on 3/8/15.
+//  Copyright (c) 2015 mikeravenelle. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
